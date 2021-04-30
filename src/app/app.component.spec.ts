81a8aeaabe2098badfd598de584abed92bb0de24
PacoCruz1 <frcruza@hotmail.com>
2021-04-30 08:59:16 +0200

    Subir cambios

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('AppComponent', () => 
+{
+  let component: AppComponent;
+  let expected = "";
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -11,25 +15,92 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+
+      
     }).compileComponents();
+
+    component = new AppComponent;
   });
 
-  it('should create the app', () => {
+  /*it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DeutscheBank'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('DeutscheBank');
-  });
+  it(`Text placeholder input 'Enter a number'`, () => {
+     const fixture = TestBed.createComponent(AppComponent);
+     const app = fixture.componentInstance;
+     expect(app.placeholder).toEqual('Enter a number');
+  });*/
 
-  it('should render title', () => {
+  it('should render Title', (() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('DeutscheBank app is running!');
+    component.ngOnInit();
+    expect(compiled.querySelector('h1').textContent).toContain('Deutsche Bank');
+  }));
+
+  xit("Text title app 'Deutsche Bank'", () =>
+  {
+    let myVar: string = component.title;
+
+    component.ngOnInit();
+
+    expect(myVar).toEqual('Deutsche Bank');
   });
+
+  it("Text title Placeholder 'Enter a number'", () =>
+  {
+    let myVar: string = component.placeholder;
+
+    expect(myVar).toEqual('Enter a number');
+  });
+
+  xit("Function Div", (() =>{
+    let a: number = 50;
+    let b: number = 10;
+    let resultt: number = a / b;
+
+    expect(component.Div()).toBe(resultt);
+  }));
+  
+  it("Is called method 'OnKey'", () =>
+  {    
+      const keyBoard = new KeyboardEvent('key');
+      let spy1 = spyOn(component, `OnKey`);
+
+      component.OnKey(keyBoard);
+
+      expect(spy1).toHaveBeenCalled();
+  });
+
+  it("Is called method 'Div'", () =>
+  {    
+      let spy1 = spyOn(component, `Div`);
+
+      component.Div();
+
+      expect(spy1).toHaveBeenCalled();
+  });
+
+  it("Is called method 'Validation'", () =>
+  {    
+      const keyBoard = new KeyboardEvent('key');
+      let spy1 = spyOn(component, `Validation`);
+
+      component.Validation(keyBoard);
+
+      expect(spy1).toHaveBeenCalled();
+  });
+
+  xit("Exists element HTML 'input'", (() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    
+    expect(compiled.querySelector('.box1').textContent).toEqual('Deutsche Bank');
+  }));
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,118 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+
+enum error
+{
+  required = "This field is empty",
+  div0 = "Do not divide by 0"
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./CSS/app.component.css']
 })
-export class AppComponent {
-  title = 'DeutscheBank';
+
+
+
+export class AppComponent  implements OnInit, OnDestroy
+{  
+  public title: string;
+  public placeholder: string = 'Enter a number';
+  public text1: string;
+  public text2: string;
+  public result: string;
+  public message1: error;
+  public message2: error;
+
+  public secons: number;
+  public input1: number;
+  public input2: number; 
+
+  public isBlur: boolean;
+  public isFocus: boolean; 
+  public isError1:boolean;
+  public isError2:boolean;
+
+  private id: any;
+
+  constructor(){};
+
+  ngOnInit()
+  {
+      this.title = 'Deutsche Bank';
+      this.result = "";
+      this.isBlur= false;
+      this.isFocus = false; 
+      this.isError1 = false;     
+      this.isError2 = false; 
+  }
+
+
+  private Tick():void
+  {
+    if(--this.secons < 0) 
+    {
+      clearInterval(this.id);
+      this.Div(); 
+    }
+  }
+  
+  public MouseEnter(e)
+  {
+    this.isFocus = false;
+    this.isBlur = false;
+  }
+
+  public OnKey(e):void
+  {        
+    this.isFocus = false;
+    this.isBlur = false;
+
+    if(e.key !== "e" && e.target.id === "box2")
+    {
+      this.Countdown();
+    }
+  }
+
+  public Validation(e):void
+  {
+    if(e.target.value === "")
+    {
+      this.message1 = error.required;
+    }
+    this.isError1 = false;
+  }
+
+  private Countdown():void
+  {
+    clearInterval(this.id);
+    this.secons = 5;
+    this.id = setInterval(() => this.Tick(), 1000);
+  }
+
+  public Div():string
+  {
+    if(this.input2 == 0)
+    {
+      this.message2 = error.div0;
+    }
+    else
+    {
+      this.isError2 = false;
+      let res: number = this.input1 / this.input2
+      this.result = res.toString();
+      
+      this.isBlur = true;
+      this.isFocus = true;
+
+      return this.result;
+    }
+
+    
+  }
+
+  ngOnDestroy()
+  {
+    clearInterval(this.id);
+  }
 }
